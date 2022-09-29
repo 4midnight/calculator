f563dcee662c21b61a40b0ad84775e4e57aa2fba
4midnight <looncats@gmail.com>
2022-09-29 05:02:05 +0300

    Build project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./pages/**/*.{html,js}", "./components/**/*.{html,js}"],
   theme: {
     extend: {
       backgroundColor: {
-        "mainColor": '#F3F3F4',
-        "blackColor": '#111111',
-        "grayColor": '#575757',
-        "orangeColor": "#FF9514",
-        "lightGrayColor": '#E5E5E5'
-      }, 
+        mainColor: "#F3F3F4",
+        blackColor: "#111111",
+        grayColor: "#575757",
+        orangeColor: "#FF9514",
+        lightGrayColor: "#E5E5E5",
+      },
       colors: {
-        "mainColor": '#F3F3F4',
-        "blackColor": '#111111',
-        "grayColor": '#575757',
-        "orangeColor": "#FF9514",
-        "lightGrayColor": '#E5E5E5'
-      }
+        mainColor: "#F3F3F4",
+        blackColor: "#111111",
+        grayColor: "#575757",
+        orangeColor: "#FF9514",
+        lightGrayColor: "#E5E5E5",
+      },
     },
   },
   plugins: [],
-}
+};
